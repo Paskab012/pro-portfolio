dc345e8eb9e273c0de3290611d6ae1034849b988
Paskab012 <pascalkabikampan@gmail.com>
2022-08-29 00:17:38 +0200

    Logo image to the main page

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import gsap from 'gsap-trial';
 import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
-import LogoS from '../../../assets/images/logo-s.png';
+import LogoS from '../../../assets/images/pic.jpg';
 import './index.scss';
 
 const Logo = () => {
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import Logo from './Logo';
 
 function Home() {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    const nameArray = ['', ' P', 'a', 's', 'c', 'a', 'l', ' ', 'k', 'a', 'b', 'i', 'k', 'a'];
+    const jobArray = [
+        'F',
+        'u',
+        'l',
+        'l',
+        '-',
+        's',
+        't',
+        'a',
+        'c',
+        'k',
+        ' ',
+        'd',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+    ];
+
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //         setLetterClass('text-animate-hover');
+    //     }, 3000);
+    // }, []);
     return (
         <>
             <div className="container home-page">
                 <div className="text-zone">
                     <h1>
-                        Hi, <br /> I am
-                        <img src={LogoTitle} alt="software developer" />
-                        Pascal Kabika Mpangirwa
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
                         <br />
+                        <span className={`${letterClass} _14`}>I</span>
+                        <span className={`${letterClass} _15`}>'m</span>
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={20} />
+                        <br />
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={26} />
                     </h1>
-                    <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+                    <h2>
+                        React/ Redux/ Typescript/ NodeJs/ Ruby on rails/ Django <br />
+                        <br />
+                        I’m a full-stack software developer! I can help you build a product ,
+                        feature or website. Look through some of my work and experience! If you like
+                        what you see and have a project you need coded, don’t hestiate to contact
+                        me.
+                    </h2>
                     <Link to="/contact" className="flat-button">
                         CONTACT ME
                     </Link>
                 </div>
+                <Logo />
             </div>
         </>
     );
