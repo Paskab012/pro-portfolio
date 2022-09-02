053ca9ba03e8156441413c4270f3fb63b87eff80
Paskab012 <pascalkabikampan@gmail.com>
2022-09-02 15:22:56 +0200

    Implement the email.js feature

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
-import './App.scss'
-
+import './App.scss';
 
 import Layout from './components/Layout';
+import Contact from './components/Contact';
 function App() {
     return (
         <Router>
@@ -13,6 +13,7 @@ function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
                 </Route>
             </Routes>
         </Router>
