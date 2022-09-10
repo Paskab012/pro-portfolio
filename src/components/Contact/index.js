ff58b6524c020c34ad22c9fe9e036b4217361cd2
Paskab012 <pascalkabikampan@gmail.com>
2022-09-10 23:59:08 +0200

    Add map location to the project

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -98,10 +98,10 @@ const Contact = () => {
                     <span>pascalkabika123@gmail.com</span>
                 </div>
                 <div className="map-wrap">
-                    <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+                    <MapContainer center={[-1.9698082, 30.1633917]} zoom={13}>
                         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                        <Marker position={[44.96366, 19.61045]}>
-                            <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+                        <Marker position={[-1.9698082, 30.1633917]}>
+                            <Popup>Find Pascal Kabika here :)</Popup>
                         </Marker>
                     </MapContainer>
                 </div>
